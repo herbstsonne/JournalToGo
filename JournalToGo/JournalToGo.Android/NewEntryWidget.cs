5198781f3959fa92b053e6c0934fbaa42355211d
mp <miriam.pfaffenbach@brz.eu>
2021-07-14 21:12:53 +0200

    add widget for new entry - part 1

---
+++ JournalToGo/JournalToGo.Android/NewEntryWidget.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Appwidget;
+using Android.Content;
+
+namespace JournalToGo.Droid
+{
+    [BroadcastReceiver(Label = "@string/widget_name")]
+    [IntentFilter(new string [] { "android.appwidget.action.APPWIDGET_UPDATE" })]
+    [MetaData("android.appwidget.provider", Resource = "@xml/widget_word")]
+    public class NewEntryWidget : AppWidgetProvider
+    {
+        public override void OnUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+        {
+            context.StartService(new Intent(context, typeof(UpdateService)));
+        }
+    }
+}
